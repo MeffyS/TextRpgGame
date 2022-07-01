e9b7e64c6ef43fc900cf0476a2532dfb9e892362
MeffyS <patryk.czop@gmail.com>
2022-07-01 20:38:26 +0200

    Bank ended

--- test_bank.py
+++ test_bank.py
@@ -8,30 +8,15 @@ class BankOptions(Enum):
     POCKET = auto()
     QUIT = "Q"
 
-class BankWithdraw:
 
-    def withdraw():
-        print(f"You choosed option WITHDRAW")
-        withdraw = input(f"How many coins you want withdrawn?")
-        try:
-            if int(withdraw) > BankBalance.balance:
-                print(f"You dont have {int(withdraw)} in bank. In Your bank balance you got {BankBalance.balance}")
-            elif int(withdraw) == 0:
-                print(f"You cannot withdrawn 0 coins")
-            elif int(withdraw) < 0:
-                print(f"You cannot withrdawn coins with negative value")
-            elif int(withdraw) <= BankBalance.balance:
-                BankBalance.balance -= int(withdraw)
-                player_backpack.coins += int(withdraw)
-                print(f"You withdraw to bank {withdraw} coins. You have a {BankBalance.balance} coins in pocket")
-        except ValueError:
-            if withdraw == "Q":
-                print("exit")
-            else:
-                print("You cannot enter a letters, except [Q] ")
 
-class BankDeposit:
-    def deposit():
+class NewBank:
+    def __init__(self, balance=0):
+        self.balance = balance
+        
+    
+
+    def deposit(self):
         print(f"You choosed option DEPOSIT")
         while True:
             deposit = input(f"How many coins you want deposit?")
@@ -43,7 +28,7 @@ class BankDeposit:
                 elif int(deposit) < 0:
                     print(f"You cannot deposit coins with negative value")
                 elif int(deposit) <= player_backpack.coins:
-                    BankBalance.balance += int(deposit)
+                    self.balance += int(deposit)
                     player_backpack.coins -= int(deposit)
                     print(f"You deposit to bank {deposit} coins. You have a {player_backpack.coins} coins in pocket")
                 break
@@ -53,36 +38,56 @@ class BankDeposit:
                 else:
                     print("You cannot enter a letters, except [Q] ")
 
+    def withdraw(self):
+        print(f"You choosed option WITHDRAW")
+        withdraw = input(f"How many coins you want withdrawn?")
+        try:
+            if int(withdraw) > self.balance:
+                print(f"You dont have {int(withdraw)} in bank. In Your bank balance you got {self.balance}")
+            elif int(withdraw) == 0:
+                print(f"You cannot withdrawn 0 coins")
+            elif int(withdraw) < 0:
+                print(f"You cannot withrdawn coins with negative value")
+            elif int(withdraw) <= self.balance:
+                self.balance -= int(withdraw)
+                player_backpack.coins += int(withdraw)
+                print(f"You withdraw to bank {withdraw} coins. You have a {self.balance} coins in pocket")
+        except ValueError:
+            if withdraw == "Q":
+                print("exit")
+            else:
+                print("You cannot enter a letters, except [Q] ")
 
-class BankBalance:
-    balance = 10
+    def bank_balance(self):
+        return print(f"Dear player, your total bank balance is {self.balance} coins")
 
-    def bank_balance():
-        print(f"Dear player, your total bank balance is {BankBalance.balance} coins")
-
-class BankPocket:
-
+    @staticmethod
     def pocket_balance():
         print(f"Dear player, your total pocket balance is {player_backpack.coins} coins")
 
+    
+    def bank(self):
+        while True:
+            for option in BankOptions:
+                print(f"[{option.value}] {option.name}")
+            bank_choice = input("Please enter ur choice") 
+            try:
+                if bank_choice == BankOptions.DEPOSIT.name or int(bank_choice) == BankOptions.DEPOSIT.value:
+                    self.deposit()
+                elif bank_choice == BankOptions.WITHDRAW.name or int(bank_choice) == BankOptions.WITHDRAW.value:
+                    self.withdraw()
+                elif bank_choice == BankOptions.BALANCE.name or int(bank_choice) == BankOptions.BALANCE.value:
+                    self.bank_balance()
+                elif bank_choice == BankOptions.POCKET.name or int(bank_choice) == BankOptions.POCKET.value:
+                    NewBank.pocket_balance()
+            except ValueError:
+                if bank_choice == "Q":
+                    break
+                else:
+                    print("You cannot enter a letters except [Q]")  
+        
 
-class Bank:
-    for option in BankOptions:
-        print(f"[{option.value}] {option.name}")
-    while True:
-        bank_choice = input("Please enter ur choice") 
-        try:
-            if bank_choice == BankOptions.DEPOSIT.name or int(bank_choice) == BankOptions.DEPOSIT.value:
-                BankDeposit.deposit()
-            elif bank_choice == BankOptions.WITHDRAW.name or int(bank_choice) == BankOptions.WITHDRAW.value:
-                BankWithdraw.withdraw()   
-            elif bank_choice == BankOptions.BALANCE.name or int(bank_choice) == BankOptions.BALANCE.value:
-                BankBalance.bank_balance()
-            elif bank_choice == BankOptions.POCKET.name or int(bank_choice) == BankOptions.POCKET.value:
-                BankPocket.pocket_balance()
-        except ValueError:
-            if bank_choice == "Q":
-                break
-            else:
-                print("You cannot enter a letters except [Q]")
-
+b = NewBank()
+b.bank()
+print(player_backpack.coins)
+print(b.balance)
