f9c543a0bf5c517df90e68a27c3563b55a4b6a2a
MeffyS <patryk.czop@gmail.com>
2022-10-20 22:53:27 +0200

    test_game_add_points ended, work fine. Player can choose attr using name or digit

--- test_game_add_points.py
+++ test_game_add_points.py
@@ -1,7 +1,6 @@
 from enum import Enum
 from game_clear_function import clearConsole
 from game_character import player
-import sys
 
 
 class Points(Enum):
@@ -14,22 +13,23 @@ class Points(Enum):
     LUCKY = [6, player.lucky]
     QUIT = [7, "Q"]
 
-
 attribute_dict = {}
 
 
 def add_points(attribute):
-    points_count = input(f"How many {player.points} you want spend?")
-    player[attribute] += int(points_count)
-    return player[attribute]
-
-    # return player[attribute]
-
-
-# add_points()
-# print(player.max_health)
-# add_points('s')
-# print("hp", player.mana)
+    while True:
+        points_count = input(f"How many {player.points} you want spend?")
+        try:
+            if int(points_count) <= player.points and int(points_count) > 0:
+                player[attribute] += int(points_count)
+                return player[attribute]
+            else:
+                print(
+                    f"Invalid value points {points_count}. Your current status of points {player.points} "
+                )
+        except ValueError:
+            print("Entered value cannot be a letter sequence")
+            continue
 
 
 def player_attributes(move):
@@ -39,52 +39,48 @@ def player_attributes(move):
         for number, attr in enumerate(Points):
             print(number, attr.name, attr.value[1])
             attribute_dict[attr.value[0]] = attr.name
+        
+        while True:
+            attribute = input("Enter attribute which you want upgrade ")
 
-        attribute = input("Enter attribute which you want upgrade ")
+            if attribute == "Q" or attribute == "q":
+                break
 
-        if attribute == "Q" or attribute == "q":
-            print("EXIT")
+            if attribute == Points.HEALTH.name or attribute == str(Points.HEALTH.value[0]):
+                attribute = "max_health"
+                add_points(attribute)
 
-        if attribute == Points.HEALTH.name or attribute == Points.HEALTH.value[0]:
-            attribute = "max_health"
-            add_points(attribute)
-            print(player.max_health)
+            elif attribute == Points.MANA.name or attribute == str(Points.MANA.value[0]):
+                attribute = "max_mana"
+                add_points(attribute)
 
-        elif attribute == Points.MANA.name or int(attribute) == Points.MANA.value[0]:
-            attribute = "max_mana"
-            print(attribute)
-        elif (
-            attribute == Points.STAMINA.name
-            or int(attribute) == Points.STAMINA.value[0]
-        ):
-            attribute = "max_stamina"
-        elif (
-            attribute == Points.DEFENCE.name
-            or int(attribute) == Points.DEFENCE.value[0]
-        ):
-            pass
-        elif (
-            attribute == Points.ATTACK.name or int(attribute) == Points.ATTACK.value[0]
-        ):
-            attribute = "max_attack"
-        elif attribute == Points.MAGIC.name or int(attribute) == Points.MAGIC.value[0]:
-            pass
-        elif attribute == Points.LUCKY.name or int(attribute) == Points.LUCKY.value[0]:
-            pass
+            elif attribute == Points.STAMINA.name or attribute == str(
+                Points.STAMINA.value[0]
+            ):
+                attribute = "max_stamina"
+                add_points(attribute)
 
-            # if attribute.isdigit() is True:
-            #     upgrade = input(f"Do you want upgrade a {attribute_dict.get(int(attribute))}")
-            # else:
-            #     upgrade = input(f"Do you want upgrade a {attribute} ")
-            #     if upgrade == "Y":
-            #         points_count = input(f"How many {player.points} you want spend?")
-            #         if player.points >= int(points_count):
-            #             player[attribute.lower()] += int(points_count)
-            #             print(player[attribute.lower()])
-            #             return player[attribute.lower()]
-        # elif attribute == Points.MANA.name or int(attribute) == Points.MANA.value[0]:
-        #     print(f"Do you want upgrade a {attribute_dict.get(int(attribute))}")
+            elif attribute == Points.DEFENCE.name or attribute == str(
+                Points.DEFENCE.value[0]
+            ):
+                attribute = "defence"
+                add_points(attribute.lower())
+
+            elif attribute == Points.ATTACK.name or attribute == str(
+                Points.ATTACK.value[0]
+            ):
+                attribute = "max_attack"
+                add_points(attribute)
+
+            elif attribute == Points.MAGIC.name or attribute == str(Points.MAGIC.value[0]):
+                attribute = "magic"
+                add_points(attribute.lower())
+
+            elif attribute == Points.LUCKY.name or attribute == str(Points.LUCKY.value[0]):
+                attribute = "lucky"
+                add_points(attribute.lower())
+            elif attribute == Points.QUIT.name or attribute == str(Points.QUIT.value[0]):
+                break
 
 
 player_attributes("STATISTIC")
-# print(attribute_dict)
