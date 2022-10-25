372725aa5d8a81b175cf7e7a74e35a293d8f2e9e
MeffyS <patryk.czop@gmail.com>
2022-10-25 15:32:39 +0200

    Game moves work, added new functions, more readable code

--- test_game_moves.py
+++ test_game_moves.py
@@ -23,74 +23,96 @@ class GameDirections:
                     self.position -= 1
             elif self.position > 0:
                 if self.east < game_map_size.map:
-                    direction = input("Enter direction [E][U][W] NR 1")
-                    if direction == "E":
-                        self.east += 1
-                        self.west -= 1
-                        self.position += 1
-                    if direction == "W":
-                        self.east -= 1
-                        self.west += 1
-                        self.position -= 1
+                    enter_direction = input("Enter direction [E][U][W] NR 1")
+                    self.east_direction_positive(enter_direction)
                 if self.east == game_map_size.map:
-                    if direction == "E":
-                        if self.east == self.position:
-                            print("You can't go any further")
-                            print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
-                            direction = input("Enter direction [U][W][C] NR 4")
-                    elif direction == "W":
-                        self.east -= 1
-                        self.west += 1
-                        self.position -= 1
-                    else:
-                        direction = input("Enter direction [U][W][C] NR 4")
-            
+                    self.east_direction_negative()
+                    # if direction == "E":
+                    #     if self.east == self.position:
+                    #         print("You can't go any further")
+                    #         print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
+                    #         direction = input("Enter direction [U][W][C] NR 4")
+                    # elif direction == "W":
+                    #     self.east -= 1
+                    #     self.west += 1
+                    #     self.position -= 1
+                    # else:
+                    #     direction = input("Enter direction [U][W][C] NR 4")
             elif self.position < 0:
                 if self.west < game_map_size.map:
-                    direction = input("Enter direction [E][U][W] NR 2")
-                    if direction == 'W':
-                        self.east -= 1
-                        self.west += 1
-                        self.position -= 1
-                    if direction == 'E':
-                        self.east += 1
-                        self.west -= 1
-                        self.position += 1
+                    enter_direction = input("Enter direction [E][U][W] NR 2")
+                    self.west_direction_positive(enter_direction)
+                    # if direction == "W":
+                    #     self.east -= 1
+                    #     self.west += 1
+                    #     self.position -= 1
+                    # if direction == "E":
+                    #     self.east += 1
+                    #     self.west -= 1
+                    #     self.position += 1
                 if self.west == game_map_size.map:
-                    if direction == 'W':
-                        if self.west != self.position:
-                            print("You can't go any further")
-                            print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
-                            direction = input("Enter direction [E][U][D] NR 4")
-                    if direction == 'E':
-                        self.east += 1
-                        self.west -= 1
-                        self.position += 1
-                    else:
-                        direction = input("Enter direction [E][U][D] NR 4")
-
-
+                    self.west_direction_negative()
+                    # if direction == "W":
+                    #     if self.west != self.position:
+                    #         print("You can't go any further on WEST")
+                    #         print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
+                    #         direction = input("Enter direction [E][U][D] NR 4")
+                    # if direction == "E":
+                    #     self.east += 1
+                    #     self.west -= 1
+                    #     self.position += 1
+                    # else:
+                    #     direction = input("Enter direction [E][U][D] NR 4")
 
     def east_direction_negative(self):
-        print("Enter direction ")
-        self.east -= 1
-        self.position += 1
-        print(self.position)
+        direction = input("Enter direction [U][W][C] NR 4")
+        if direction == "E":
+            if self.east == self.position:
+                print("You can't go any further on EAST")
+                print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
+                # direction = input("Enter direction [U][W][C] NR 5 nr 0")
+        elif direction == "W":
+            print(direction)
+            self.east -= 1
+            self.west += 1
+            self.position -= 1
+        else:
+            direction = input("Enter direction [U][W][C] NR 5 nr 1")
 
-    def east_direction_positive(self):
-        self.east += 1
-        self.position += 1
-        print(self.position)
+    def east_direction_positive(self, direct):
+        if direct == "E":
+            self.east += 1
+            self.west -= 1
+            self.position += 1
+        if direct == "W":
+            self.east -= 1
+            self.west += 1
+            self.position -= 1
 
     def west_direction_negative(self):
-        self.west -= 1
-        self.position += 1
-        print(self.position)
+        direction = input("Enter direction [E][U][D] NR 6 nr 1")
+        if direction == "W":
+            if self.west == abs(self.position):
+                print("You can't go any further on WEST")
+                print(f"WEST:{self.west}|{self.position}|{self.east}:EAST")
+                # direction = input("Enter direction [E][U][D] NR 6")
+        elif direction == "E":
+            print(direction)
+            self.east += 1
+            self.west -= 1
+            self.position += 1
+        else:
+            direction = input("Enter direction [E][U][D] NR 6 nr 2")
 
-    def west_direction_positive(self):
-        self.west += 1
-        self.position += 1
-        print(self.position)
+    def west_direction_positive(self, direct):
+        if direct == "W":
+            self.east -= 1
+            self.west += 1
+            self.position -= 1
+        if direct == "E":
+            self.east += 1
+            self.west -= 1
+            self.position += 1
 
 
 class GameMoves:
