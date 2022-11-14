6245dff217c2d75201650c85efabe53dffddf4f2
MeffyS <patryk.czop@gmail.com>
2022-11-15 00:59:37 +0100

    small rework game_player_chest.py, edited buy_special_slot, added buy_slot

--- game_player_chest.py
+++ game_player_chest.py
@@ -4,6 +4,10 @@ from game_character import player
 from game_clear_function import clearConsole
 
 
+player_items = ["Armor", "Boots"]
+player_level = 21
+buy_slot = 10
+
 
 tested_dict = {}
 
@@ -18,42 +22,97 @@ class SpecialChestSlot(Enum):
     CITY_SLOT = ["6", "City Slot", 10000, True]
 
 
-# for x in SpecialChestSlot:
-#     print(x.value[0])
-
-
 class CityChestOption(Enum):
-    SLOT = [0, "Buy Slot"]
-    SPECIAL_SLOT = [1, "Buy Slot"]
-    PUT_IN_ITEM = [2, "Put In Item"]
-    TAKE_OUT_ITEM = [3, "Take Out Item"]
+    SLOT = ["0", "Buy Slot"]
+    SPECIAL_SLOT = ["1", "Buy Special Slot"]
+    PUT_IN_ITEM = ["2", "Put In Item"]
+    TAKE_OUT_ITEM = ["3", "Take Out Item"]
 
 
 class CityChest:
+    bool_counter = 0
     free_chest_slot = 5
     bought_chest_slot = 0
     special_slot = {}
 
     count_chest_slot = free_chest_slot + bought_chest_slot + player.level
 
-    def buy_special_slot():
-        print("--------------------------------")
-        for slot in SpecialChestSlot.__members__.values():
-            print(slot.value[1], slot.value[3])
-        print("--------------------------------")
+    def chest():
         while True:
-            buy_slot = input("Enter slot value which you want to buy")
+            for option in CityChestOption:
+                print(f"[{option.value[0]}] [{option.value[1]}]")
+            chest_option = input("Enter value")
+            if chest_option == CityChestOption.SLOT.value[0]:
+                CityChest.buy_slot()
+            elif chest_option == CityChestOption.SPECIAL_SLOT.value[0]:
+                CityChest.buy_special_slot()
+            elif chest_option == CityChestOption.PUT_IN_ITEM.value[0]:
+                pass
 
-            for slot in SpecialChestSlot.__members__.values():
+    @classmethod
+    def buy_slot(cls):
+        print(f"SLOTS: [{cls.count_chest_slot}]")
+        print(f"Cost per next slot is {round(player_level*250)}")
+        print(player_level)
+        # # while True:
+
+    @classmethod
+    def buy_special_slot(cls):
+
+        while True:
+            for slot in SpecialChestSlot:
+                if slot.value[3] is True:
+                    print(f"[{slot.value[0]}][{slot.value[1]}] COST:[{slot.value[2]}]")
+                else:
+                    print(f"[{slot.value[0]}][{slot.value[1]}] COST:[PURCHASED]")
+
+            buy_slot = input(
+                "Enter slot value which you want to buy or [Q] to Quit "
+            ).upper()
+            for slot in SpecialChestSlot:
                 if buy_slot == slot.value[0]:
                     if slot.value[3] is True:
+                        cls.bool_counter += 1
                         slot.value[3] = False
+                        cls.special_slot[slot.value[1]] = []
+            if buy_slot == "Q":
+                break
+            if buy_slot != slot.value[0] and buy_slot not in [
+                slot.value[0] for slot in SpecialChestSlot
+            ]:
+                print("PLEASE ENTER CORRECT NUMBER")
 
-                    for slot1 in SpecialChestSlot.__members__.values():
-                        if slot1.value[3] is True:
-                            print(slot1.value[0], slot1.value[1])
-                        else:
-                            print(slot1.value[0], slot1.value[1], "PURCHASED")
+    # def add_item_to_store():
+    #     for item_number, item in enumerate(player_items):
+    #         print(f"[{item_number}] {item}")
+
+    #     player_chest = [[slot] for slot in range(0, 6 + player_level + buy_slot)]
+    #     while True:
+    #         if len(player_items) > 0:
+    #             choose_item = int(
+    #                 input("Please choose number item which you want hide in chest ")
+    #             )
+    #             if int(choose_item) < len(player_items):
+    #                 for _, chest_place in enumerate(player_chest):
+    #                     print(chest_place, end='' )
+    #                 while True:
+    #                     enter_place = input(f" <-- Choose slot on item: ")
+    #                     if int(enter_place) < len(player_chest):
+    #                         if len(player_chest[int(enter_place)]) < 2:
+    #                             player_chest[int(enter_place)].append(
+    #                                 player_items[choose_item]
+    #                             )
+    #                             del player_items[choose_item]
+    #                             print(player_items)
+    #                             print(player_chest)
+    #                             break
+    #                         else:
+    #                             print("This slot is taken, try choose other")
+    #             else:
+    #                 break
+    #         else:
+    #             break
 
 
-CityChest.buy_special_slot()
+a = CityChest
+a.chest()
